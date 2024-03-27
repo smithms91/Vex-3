14cf892d0d22fc89291e9ad5b442db34e08ba6c8
smithms91 <smithms91@gmail.com>
2024-03-27 08:25:48 -0400

    fix sign in page style

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -24,7 +24,7 @@ export default async function SignUpPage({
   }
 
   return (
-    <section className="min-h-screen max-w-[450px] mx-auto flex flex-col items-center justify-center">
+    <section className="min-h-screen max-w-[450px] mx-auto flex flex-col items-center justify-center bg-gradient-to-tl from-dark-from to-dark-to">
       <div className="relative">
         <h1 className="text-4xl mb-10 text-white font-bold tracking-widest">VEX</h1>
         <h1 className="absolute top-0 left-0 right-0 text-4xl mb-10 text-white font-bold tracking-widest blur-md opacity-50">VEX</h1>
