3ba8f07a8108713f8adafe7df449ef637a8afe2e
smithms91 <smithms91@gmail.com>
2024-04-11 12:24:37 -0400

    fixed profile/add contact button

--- src/app/[username]/_components/user-add-contact.tsx
+++ src/app/[username]/_components/user-add-contact.tsx
@@ -1,40 +1,46 @@
-'use client';
+"use client";
 
-import { Button } from '@/components/ui/button'
-import { User } from '@/types';
-import React from 'react'
-import { profileColors } from '@/constants';
+import { Button } from "@/components/ui/button";
+import { User } from "@/types";
+import React from "react";
+import { profileColors } from "@/constants";
 
 type Props = {
   user: User;
-}
+};
 
 const AddContactButton = ({ user }: Props) => {
-  console.log('use2r', user)
-  // console.log('user2', user)
-  const profileColor = profileColors.find(color => color.color === user.profile_color)?.singleValue
+  const profileColor = profileColors.find(
+    (color) => color.color === user.profile_color,
+  )?.singleValue;
 
   const handleButtonClick = () => {
     const vCard = [
-      'BEGIN:VCARD',
-      'VERSION:3.0',
+      "BEGIN:VCARD",
+      "VERSION:3.0",
       `N:;${user.first_name} ${user.last_name};;;`,
       `TEL;TYPE=CELL:${user.phone_number}`,
       `EMAIL:${user.email}`,
-      'END:VCARD',
-    ].join('\n');
+      "END:VCARD",
+    ].join("\n");
 
-    const blob = new Blob([vCard], { type: 'text/vcard' });
+    const blob = new Blob([vCard], { type: "text/vcard" });
     const url = URL.createObjectURL(blob);
 
-    const link = document.createElement('a');
+    const link = document.createElement("a");
     link.href = url;
     link.download = `${user.first_name}.vcf`;
     link.click();
   };
   return (
-    <Button className="w-full py-7 z-10 -my-2 border-b-2 border-card-bg-light cursor-pointer text-md bg-card-bg-dark hover:bg-card-bg-light" style={{ backgroundColor: profileColor }} onClick={() => handleButtonClick()}>Add Contact</Button>
-  )
-}
+    <Button
+      className="w-full py-7 z-10 -my-2 border-b-2 border-card-bg-light cursor-pointer text-md bg-card-bg-dark hover:bg-card-bg-light"
+      style={{ backgroundColor: profileColor }}
+      onClick={() => handleButtonClick()}
+    >
+      Add Contact
+    </Button>
+  );
+};
 
-export default AddContactButton
+export default AddContactButton;
--- src/components/edit-profile-button.tsx
+++ src/components/edit-profile-button.tsx
@@ -1,22 +1,29 @@
-'use client';
+"use client";
 
-import { useRouter } from 'next/navigation';
-import React from 'react'
-import { Button } from './ui/button';
-import { usePreviewMode } from './context/preview-mode-provider';
-import AddContactButton from '@/app/[username]/_components/user-add-contact';
-import { User } from '@/types';
+import { useRouter } from "next/navigation";
+import React from "react";
+import { Button } from "./ui/button";
+import { usePreviewMode } from "./context/preview-mode-provider";
+import AddContactButton from "@/app/[username]/_components/user-add-contact";
+import { User } from "@/types";
 
 type Props = {
-  user: any;
-}
+  user: User;
+};
 
-const EditProfileButton = ({ user }: Props) => {
-  const router = useRouter()
+const EditProfileButton = ({ user }: { user: User }) => {
+  const router = useRouter();
   const { preview } = usePreviewMode();
-  return (
-    !preview ? <Button className="z-10 py-7 -my-2 text-md bg-card-bg-dark hover:bg-card-bg-light w-full border-b-2 border-card-bg-light cursor-pointer" onClick={() => router.push('/account/setup')}>Edit Profile</Button> : <AddContactButton user={user.data[0] as User} />
-  )
+  if (!preview) {
+    return <Button
+      className="z-10 py-7 -my-2 text-md bg-card-bg-dark hover:bg-card-bg-light w-full border-b-2 border-card-bg-light cursor-pointer"
+      onClick={() => router.push("/account/setup")}
+    >
+      Edit Profile
+    </Button>
+  } else {
+    return <AddContactButton user={user} />
+  };
 }
 
-export default EditProfileButton
+export default EditProfileButton;
