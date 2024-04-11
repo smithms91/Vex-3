c59b1c717f8b86dfae6201c6522a15bc1f29a304
smithms91 <smithms91@gmail.com>
2024-04-11 18:43:44 -0400

    trying to fix paid footer

--- src/app/[username]/_components/paid-footer.tsx
+++ src/app/[username]/_components/paid-footer.tsx
@@ -13,35 +13,14 @@ type Props = {
 
 const PaidFooter = ({ darkText }: Props) => {
   const { preview, setPreview } = usePreviewMode();
-  const { color, setColor } = useThemeColor();
 
   if (!preview) return null;
 
   return (
     <footer className="z-50 mt-auto border-t-2 border-slate-600/25 bg-[#12223f]/20 text-white max-w-[450px] w-full p-4 text-center mx-auto">
-      <h1
-        className={cn(
-          "text-2xl",
-          color == "light" ? "text-black" : "text-white",
-        )}
-      >
-        VEX
-      </h1>
-      <p
-        className={cn(
-          "text-xs mb-1 text-black",
-          color == "light" ? "text-black" : "text-white",
-        )}
-      >
-        The Future of Business Cards
-      </p>
-      <Link
-        className={cn(
-          "text-xs flex items-center justify-center underline",
-          color == "light" ? "text-black" : "text-white",
-        )}
-        href="/"
-      >
+      <h1 className="text-2xl">VEX</h1>
+      <p className="text-xs mb-1 text-black">The Future of Business Cards</p>
+      <Link className="text-xs flex items-center justify-center underline" href="/">
         Learn More <ArrowRight size={12} className="mt-[.15rem]" />
       </Link>
     </footer>
